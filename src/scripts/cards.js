b846e3bcf6f4346770794497174dc5365d791ea9
Artem Dementjev <artemios.dementjev@yandex.kz>
2024-05-16 14:20:19 +0300

    Webpack settings complete

---
+++ src/scripts/cards.js
@@ -0,0 +1,18 @@
+export function createCard(cardObject, deleteFunction) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  const cardImage = cardElement.querySelector('.card__image');
+  cardImage.setAttribute('src', cardObject.link);
+  cardImage.setAttribute('alt', cardObject.name);
+  cardElement.querySelector('.card__title').textContent = cardObject.name;
+  deleteButton.addEventListener('click', () => deleteFunction(cardElement));
+  return cardElement;
+}
+
+export function deleteCard(card) {
+  card.remove()
+}
+
+
+
---
+++ src/scripts/modal.js
@@ -0,0 +1,7 @@
+export function openModal(element) {
+
+};
+
+export function closeModal(element) {
+
+};
